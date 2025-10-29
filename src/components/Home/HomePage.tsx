c876682080d01936320200b4fa7c0586e5ce8100
soroush4208 <soroush.r80@gmail.com>
2025-10-29 15:15:12 +0330

    ref : refactor code

--- src/components/Home/HomePage.tsx
+++ src/components/Home/HomePage.tsx
@@ -1,7 +1,44 @@
-import React from 'react';
+'use client';
+
+import MotionSection from '../shared/Motion/MotinSection';
+import SwiperComponent from '../shared/Swiper/Swiper';
 
 function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <SwiperComponent />
+
+      <MotionSection
+        title="عسل طبیعی از ما بخواه!"
+        color="text-red-500"
+        variant="fadeUp"
+      />
+
+      <div className="text-2xl w-full h-80 bg-amber-200 flex items-center justify-center">
+        سکشن
+      </div>
+
+      <MotionSection
+        title="کیفیت بالا، طعم بی‌نظیر!"
+        color="text-green-500"
+        variant="fadeLeft"
+      />
+
+      <div className="text-2xl w-full h-80 bg-amber-200 flex items-center justify-center">
+        سکشن
+      </div>
+
+      <MotionSection
+        title="از دل طبیعت تا سفره شما"
+        color="text-blue-500"
+        variant="fadeRight"
+      />
+
+      <div className="text-2xl w-full h-80 bg-amber-200 flex items-center justify-center">
+        سکشن
+      </div>
+    </div>
+  );
 }
 
 export default HomePage;
--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
 import Logo from '@/assets/image/logo.png';
 import Image from 'next/image';
 function Header() {
   return (
-    <div>
-      <Image src={Logo} alt="logo" />
+    <div className="flex flex-row-reverse items-center justify-between">
+      <Image src={Logo} alt="logo" className="w-10" />
       header
     </div>
   );
